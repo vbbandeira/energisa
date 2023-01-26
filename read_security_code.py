39ecb15c150cc2af7f8b2d66f5a805c7f947c0e7
Vinícius Bandeira <vbritobandeira@gmail.com>
2023-01-25 23:14:38 -0300

    comment celery use in implementation

--- read_security_code.py
+++ read_security_code.py
@@ -1,11 +1,16 @@
-from celery import Celery
-import imaplib
+# from celery import Celery
 import email
+import imaplib
+import time
+
+# Set the broker URL
+# CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 
 # Create a new Celery instance
-app = Celery('tasks', broker='redis://localhost:6379/0')
+# app = Celery('tasks', broker=CELERY_BROKER_URL)
 
-@app.task
+
+# @app.task
 def check_email():
     # Connect to the email server
     conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
@@ -15,7 +20,8 @@ def check_email():
     conn.select("inbox")
 
     # Search for emails with the specific subject
-    status, messages = conn.search(None, '(SUBJECT "Fwd: Código de segurança da Energisa")')
+    status, messages = conn.search(
+        None, '(SUBJECT "Fwd: Código de segurança da Energisa")')
 
     # Iterate through the emails
     for msg_id in messages[0].split():
@@ -24,13 +30,11 @@ def check_email():
         for response in msg_data:
             if isinstance(response, tuple):
                 msg = email.message_from_bytes(response[1])
-                # Get the email address of the sender
-                email_address = msg['From']
+
                 # Get the security code
                 security_code = msg.get_payload()[0].get_payload()
                 # Get the name from the email content
                 name = msg.get_payload()[1].get_payload()
-                print("From:", email_address)
                 print("Name:", name)
                 print("Security Code:", security_code)
 
@@ -38,10 +42,16 @@ def check_email():
     conn.close()
     conn.logout()
 
-# Schedule the check_email task to run every 30 seconds
-app.conf.beat_schedule = {
-    'check_email': {
-        'task': 'tasks.check_email',
-        'schedule': 30.0,
-    },
-}
+
+# Schedule the check_email task to run every 60 seconds
+# app.conf.beat_schedule = {
+#     'check_email': {
+#         'task': 'tasks.check_email',
+#         'schedule': 1.0,
+#     },
+# }
+
+# run the check_email function in an infinite loop
+while True:
+    check_email()
+    time.sleep(30)  # Wait 30 seconds
